ab584bd4c35263f41781fbcf2e4b07a4fdc22f66
EndrexAkoto <martinakoto3@gmail.com>
2024-02-02 18:46:18 +0300

    task_0

---
+++ 0x00-Webpack/task_0/src/index.js
@@ -0,0 +1,8 @@
+// src/index.js
+import $ from 'jquery'
+
+$(document).ready(() => {
+    $('body').append('<p>Paragraph 1</p>')
+    $('body').append('<p>Paragraph 2</p>')
+    $('body').append('<p>Paragraph 3</p>')
+})
