9020eb8827f4ee9a8bfc9c81e424f3df58482d58
EndrexAkoto <martinakoto3@gmail.com>
2024-02-05 22:25:29 +0300

    changes

---
+++ 0x01-react_intro/src/App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1>Your React App</h1>
+      {/* Add your components and content here */}
+    </div>
+  )
+}
+
+export default App
