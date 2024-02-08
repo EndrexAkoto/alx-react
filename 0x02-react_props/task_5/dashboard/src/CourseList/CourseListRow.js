86577662ee7405dafa47acd4c9219f9dfce86ca2
EndrexAkoto <martinakoto3@gmail.com>
2024-02-08 14:26:35 +0300

    task_5

---
+++ 0x02-react_props/task_5/dashboard/src/CourseList/CourseListRow.js
@@ -0,0 +1,38 @@
+import { checkPropTypes } from 'prop-types'
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const CourseListRow = ({isHeader, textFirstCell, textSecondCell}) => {
+    if (isHeader) {
+        if (textSecondCell === null || textSecondCell === undefined) {
+            return (
+                <th colSpan="2">{ textFirstCell }</th>
+            )
+        } else {
+            return (
+                <tr>
+                    <th>{ textFirstCell }</th>
+                    <th>{ textSecondCell }</th>
+                </tr>
+            )
+        }
+    } else {
+        <tr>
+            <td>{ textFirstCell }</td>
+            <td>{ textSecondCell }</td>
+        </tr>
+    }
+}
+
+CourseListRow.prototype = {
+    isHeader: checkPropTypes.bool,
+    textFirstCell: PropTypes.string.isRequired,
+    textSecondCell: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+}
+
+CourseListRow.defaultProps = {
+    isHeader: false,
+    textSecondCell: null,
+}
+
+export default CourseListRow
