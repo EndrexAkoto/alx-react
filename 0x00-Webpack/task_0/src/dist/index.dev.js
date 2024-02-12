45de14a7df0d695bce6249931fbe26e1ed51305a
EndrexAkoto <martinakoto3@gmail.com>
2024-02-12 15:51:14 +0300

    task_0

--- 0x00-Webpack/task_0/src/dist/index.dev.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var _jquery = _interopRequireDefault(require("jquery"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-
-(0, _jquery["default"])('body').append('<p>Holberton Dashboard</p>');
-(0, _jquery["default"])('body').append('<p>Dashboard data for the students</p>');
-(0, _jquery["default"])('body').append('<p>Copyright - Holberton School</p>');
