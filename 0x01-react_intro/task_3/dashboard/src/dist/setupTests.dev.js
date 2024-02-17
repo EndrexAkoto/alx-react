86c774742ca219d6bbede2d8c0c511b03ebb38e9
EndrexAkoto <martinakoto3@gmail.com>
2024-02-18 00:02:17 +0300

    update

--- 0x01-react_intro/task_3/dashboard/src/dist/setupTests.dev.js
+++ 0x01-react_intro/task_3/dashboard/src/dist/setupTests.dev.js
@@ -1,3 +1,11 @@
 "use strict";
 
-require("@testing-library/jest-dom");
+var _enzyme = require("enzyme");
+
+var _enzymeAdapterReact = _interopRequireDefault(require("@wojtekmaj/enzyme-adapter-react-18"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+(0, _enzyme.configure)({
+  adapter: new _enzymeAdapterReact["default"]()
+});
