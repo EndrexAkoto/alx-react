10ed37ac6b06adbbfeeeb7cbaafa7ccb82a8110b
EndrexAkoto <martinakoto3@gmail.com>
2024-02-20 03:02:46 +0300

    update

--- 0x03-React_component/task_1/dashboard/src/App/App.test.js
+++ 0x03-React_component/task_1/dashboard/src/App/App.test.js
@@ -3,8 +3,7 @@ import App from './App'
 import Header from '../Header/Header'
 import Login from '../Login/Login'
 import Footer from '../Footer/Footer'
-import { shallow, mount } from 'enzyme'
-
+import { shallow } from 'enzyme'
 
 describe('<App />', () => {
     it('App renders without crashing', () => {
@@ -19,7 +18,7 @@ describe('<App />', () => {
     })
     
     it('App contains Login component', () => {
-        const wrapper = shallow(<App isLoggedIn={false} />)
+        const wrapper = shallow(<App />)
         const component = wrapper.find(Login)
         expect(component.exists()).toBe(true)
     })
@@ -30,4 +29,14 @@ describe('<App />', () => {
         expect(component.exists()).toBe(true)
     })
 
+    it('Calls logOut and displays alert when control and h keys are pressed', () => {
+        const logOutMock = jest.fn()
+        const wrapper = shallow(<App logOut={logOutMock} />)
+        
+        const event = new KeyboardEvent('keydown', { key: 'h', ctrlKey: true })
+        document.dispatchEvent(event)
+
+        expect(logOutMock).toHaveBeenCalled()
+        expect(window.alert).toHaveBeenCalledWith('Logging you out')
+    })
 })
