e4039faab5d34a08f5cef7cba1edf2dbbe2f30d5
EndrexAkoto <martinakoto3@gmail.com>
2024-02-21 22:12:03 +0300

    task_0

--- 0x04-React_inline_styling/task_0/dashboard/src/CourseList/CourseList.js
+++ 0x04-React_inline_styling/task_0/dashboard/src/CourseList/CourseList.js
@@ -1,11 +1,10 @@
 import React from "react";
 import CourseListRow from "./CourseListRow";
-import './CourseList.css'
-import  CourseShape  from "./CourseShape";
-import PropType from 'prop-types'
+import "./CourseList.css";
+import CourseShape from "./CourseShape";
+import PropTypes from "prop-types";
 
-
-const CourseList = ({listCourses}) => {
+const CourseList = ({ listCourses }) => {
   return (
     <table id="CourseList">
       <thead>
@@ -17,32 +16,34 @@ const CourseList = ({listCourses}) => {
         />
       </thead>
       <tbody>
-        {
-          listCourses.length > 0 ? 
-          (<>
-            {
-              
-              listCourses.map((value,index)=>{
-                return (<CourseListRow key={value.id} textFirstCell={value.name} textSecondCell={value.credit} />)
-              })
-            }
-          </>):
-          (<>
-            <CourseList textFirstCell="No course available yet" />
-          </>)
-        }
-        
+        {listCourses.length > 0 ? (
+          <>
+            {listCourses.map((value, index) => {
+              return (
+                <CourseListRow
+                  key={value.id}
+                  textFirstCell={value.name}
+                  textSecondCell={value.credit}
+                />
+              );
+            })}
+          </>
+        ) : (
+          <>
+            <CourseListRow textFirstCell="No course available yet" />
+          </>
+        )}
       </tbody>
     </table>
   );
 };
 
-CourseList.prototype = {
-  listCourses: PropType.arrayOf(CourseShape),
+CourseList.propTypes = {
+  listCourses: PropTypes.arrayOf(CourseShape)
 };
 
 CourseList.defaultProps = {
-  listCourses: [],
+  listCourses: []
 };
 
 export default CourseList;
