39402bc7fa6393c25c3c2b42d5d2c506238832ef
EndrexAkoto <martinakoto3@gmail.com>
2024-02-24 19:09:57 +0300

    task_2

---
+++ 0x05-react_state/task_2/dashboard/src/Notifications/dist/NotificationItemShape.dev.js
@@ -0,0 +1,22 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _propTypes = _interopRequireDefault(require("prop-types"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var NotificationItemShape = _propTypes["default"].shape({
+  id: _propTypes["default"].number.isRequired,
+  __html: _propTypes["default"].shape({
+    html: _propTypes["default"].string
+  }),
+  type: _propTypes["default"].string.isRequired,
+  value: _propTypes["default"].string
+});
+
+var _default = NotificationItemShape;
+exports["default"] = _default;
