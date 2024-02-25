7064260ccd7e13e7b653f5d77f396cf7f26dfb62
EndrexAkoto <martinakoto3@gmail.com>
2024-02-25 17:37:22 +0300

    1-map.js

---
+++ 0x06-React_Immutable/dist/1-map.dev.js
@@ -0,0 +1,15 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _immutable = require("immutable");
+
+var getImmutableObject = function getImmutableObject(object) {
+  return (0, _immutable.Map)(object);
+};
+
+var _default = getImmutableObject;
+exports["default"] = _default;
